6dd0b2176aefbe2d49f33f72b18268b8fcdb4f3b
Filip Persson <filip.tillberg.persson@gmail.com>
2021-11-12 12:37:13 +0100

    completed the list with funcitons and css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+titles = []
+100.times do
+  titles << Faker::Movie.title
+end
+
+titles.uniq!
+
+20.times do
+  Movie.create!(
+    title: titles[0],
+    overview: Faker::Movies::HitchhikersGuideToTheGalaxy.quote,
+    poster_url: "https://source.unsplash.com/featured/?#{titles[0]}",
+    rating: (1..10).to_a.sample
+  )
+  titles.delete_at(0)
+end
